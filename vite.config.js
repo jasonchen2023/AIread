38cad860347b3855737662617e064c386347f75e
nsyusen <nsyusen@yahoo.com>
2023-04-11 13:41:45 -0400

    favicon plugin addition

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
+import ViteFaviconsPlugin from 'vite-plugin-favicon';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint()],
+  plugins: [
+    eslint(),
+    // eslint-disable-next-line new-cap
+    ViteFaviconsPlugin('./src/img/kipper.png'),
+  ],
   css: {
     postcss: {
       plugins: [
