7aa849d5b278da0ffea7da07baac571478105e1a
Jason Chen <jason.chen.23@dartmouth.edu>
2024-01-07 21:18:53 -0800

    implement file delete confirmation

--- src/components/homePage/FileList.jsx
+++ src/components/homePage/FileList.jsx
@@ -1,13 +1,40 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { confirmAlert } from 'react-confirm-alert';
 import { deleteFile } from '../../services/firebase';
 import trash from '../../img/trash.png';
+import 'react-confirm-alert/src/react-confirm-alert.css';
 import './FileList.scss';
 
 function FileList() {
   const allFiles = useSelector((state) => state.files.allFiles);
   const [filterText, setFilterText] = useState('');
+
+  const launchModal = (id, title) => {
+    confirmAlert({
+      // eslint-disable-next-line react/no-unstable-nested-components
+      customUI: ({ onClose }) => {
+        return (
+          <div className="alert">
+            <div className="alert-title">Confirm deletion of {title}?</div>
+            <div className="confirm-delete-buttons">
+              <button type="button"
+                onClick={() => {
+                  deleteFile(id, title);
+                  onClose();
+                }}
+                className="alert-delete-btn"
+              >
+                Delete
+              </button>
+              <button type="button" onClick={onClose} className="alert-btn-no">No</button>
+            </div>
+          </div>
+        );
+      },
+    });
+  };
 
   return (
     <div className="file-list">
@@ -25,7 +52,8 @@ function FileList() {
                   <NavLink to={`/reading/${el.id}`} rel="noopener noreferrer">
                     {el.title}
                   </NavLink>
-                  <img className="trash" alt="Delete" src={trash} onClick={() => { deleteFile(el.id, el.title); }} />
+                  {/* <img className="trash" alt="Delete" src={trash} onClick={() => { deleteFile(el.id, el.title); }} /> */}
+                  <img className="trash" alt="Delete" src={trash} onClick={() => launchModal(el.id, el.title)} />
                 </div>
               </div>
             </div>
